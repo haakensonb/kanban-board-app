8665cd89d7f356f3bb138be7acebe216ac3879ab
haakensonb <untamedhawk44@gmail.com>
2021-03-18 00:57:06 -0400

    Initial commit

---
+++ kanban-board/src/test/java/com/backend/kanbanboard/KanbanBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.kanbanboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
