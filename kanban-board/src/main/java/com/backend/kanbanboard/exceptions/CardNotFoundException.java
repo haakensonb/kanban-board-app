e1742875f6c090ad949ed3047a3d75d31409dc06
haakensonb <untamedhawk44@gmail.com>
2021-03-19 18:15:00 -0400

    Add default serial version id

--- kanban-board/src/main/java/com/backend/kanbanboard/exceptions/CardNotFoundException.java
+++ kanban-board/src/main/java/com/backend/kanbanboard/exceptions/CardNotFoundException.java
@@ -1,6 +1,11 @@
 package com.backend.kanbanboard.exceptions;
 
 public class CardNotFoundException extends RuntimeException {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
     public CardNotFoundException(Long id){
         super("Could not find card " + id);
     }
