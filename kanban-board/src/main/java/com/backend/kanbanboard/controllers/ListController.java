981887ce50e3b8c701da3c16fd30d743f038af49
haakensonb <untamedhawk44@gmail.com>
2021-03-23 18:17:33 -0400

    Add PUT mapping

--- kanban-board/src/main/java/com/backend/kanbanboard/controllers/ListController.java
+++ kanban-board/src/main/java/com/backend/kanbanboard/controllers/ListController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,6 +51,20 @@ public class ListController {
     @PostMapping("/lists")
     ListModel newList(@RequestBody ListModel newList){
         return listRepo.save(newList);
+    }
+
+    @PutMapping("/lists/{id}")
+    ListModel updateList(@RequestBody ListModel newList, @PathVariable Long id){
+        return listRepo.findById(id).map(list -> {
+            // Update relevant fields.
+            list.setName(newList.getName());
+            return listRepo.save(list);
+        }).orElseGet(() -> {
+            // Or create newList using id.
+            newList.setId(id);
+            return listRepo.save(newList);
+        });
+        
     }
 
     @DeleteMapping("/lists/{id}")
